98bb5b37ddc2873f482e2aa8714d850c7678a249
XaviFibla <xavi.fibla@gmail.com>
2022-01-25 14:42:57 +0100

    Added bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import { createRouter } from './router'
 import { createStore } from './store'
+
 Vue.use(Vuex)
 Vue.use(VueRouter)
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
 Vue.config.productionTip = false
+
 const router = createRouter();
 const store = createStore();
 new Vue({
